3d345b2485f10dce6d8439862e7da2ae3eecacb6
LutzenH <LutzenH@users.noreply.github.com>
2019-05-28 19:44:08 +0200

    Added zooming to OrbitCameraEntity

--- DProject/Entity/Camera/OrbitCameraEntity.cs
+++ DProject/Entity/Camera/OrbitCameraEntity.cs
@@ -7,17 +7,24 @@ namespace DProject.Entity.Camera
 {
     public class OrbitCameraEntity : CameraEntity
     {
+        private const float ScrollSpeed = 1f;
+        private const float CameraRotationSpeed = 2f;
+        private const float MinimumCameraZoom = 3f;
+        private const float MaximumCameraZoom = 20f;
+
+        private const float ToRadians = (float)(Math.PI *2d / 360f);
+
         private readonly AbstractEntity _entity;
 
         private Vector3 _cameraOffset;
         private float _distanceFromCamera = 10f;
-        float cameraOrbitInDegrees = 15f;
-
+        private float _cameraOrbitInDegrees = 0f;
+        
         public OrbitCameraEntity(AbstractEntity entityToOrbit, Quaternion rotation) : base(new Vector3(0,0,0f), rotation)
         {
             _entity = entityToOrbit;
             
-            _cameraOffset = new Vector3(0,10,_distanceFromCamera);
+            _cameraOffset = new Vector3(0,_distanceFromCamera,_distanceFromCamera);
             
             var entityPosition = _entity.GetPosition();
             Position = entityPosition + _cameraOffset;
@@ -30,12 +37,20 @@ namespace DProject.Entity.Camera
             var entityPosition = _entity.GetPosition();
 
             if (Keyboard.GetState().IsKeyDown(Keys.Left))
-                cameraOrbitInDegrees -= 2f;
+                _cameraOrbitInDegrees -= CameraRotationSpeed;
             if (Keyboard.GetState().IsKeyDown(Keys.Right))
-                cameraOrbitInDegrees += 2f;
+                _cameraOrbitInDegrees += CameraRotationSpeed;
 
-            const float toRadians = (float)(Math.PI *2d / 360f);
-            var rotationInRadians = cameraOrbitInDegrees * toRadians;
+            if (Mouse.GetState().ScrollWheelValue < Game1.PreviousMouseState.ScrollWheelValue)
+                _distanceFromCamera += ScrollSpeed;
+            else if (Mouse.GetState().ScrollWheelValue > Game1.PreviousMouseState.ScrollWheelValue)
+                _distanceFromCamera -= ScrollSpeed;
+
+            _distanceFromCamera = MathHelper.Clamp(_distanceFromCamera, MinimumCameraZoom, MaximumCameraZoom);
+            
+            _cameraOffset = new Vector3(0,_distanceFromCamera,_distanceFromCamera);
+
+            var rotationInRadians = _cameraOrbitInDegrees * ToRadians;
             
             var angleMatrix = Matrix.CreateFromAxisAngle(Vector3.Up, rotationInRadians);
             var newCameraPosition = Vector3.Transform(_cameraOffset, angleMatrix);
