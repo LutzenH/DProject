d9c8593fb472670ddc184cdccffdd18836d8e557
LutzenH <46445220+LutzenH@users.noreply.github.com>
2019-07-30 11:03:27 +0200

    Removed unused assets and added a Windows Solution

---
+++ DProject.Windows/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using DProject.Manager;
+
+namespace DProject
+{
+    public class Program
+    {   
+        [STAThread]
+        public static void Main(string[] args)
+        {
+            using (var game = new Game1(new GameEntityManager()))
+                game.Run();
+        }
+    }
+}
