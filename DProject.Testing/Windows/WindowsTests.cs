1d924c905be3d834f5fc9e0c336a9f41cdb2590f
LutzenH <46445220+LutzenH@users.noreply.github.com>
2019-12-27 19:36:31 +0100

    Add DProject.Testing.Windows

---
+++ DProject.Testing/Windows/WindowsTests.cs
@@ -0,0 +1,11 @@
+﻿using DProject.Testing.Core;
+using NUnit.Framework;
+
+namespace DProject.Testing.Windows
+{
+    /// <summary>
+    /// NUnit test class that contains all Windows specific tests.
+    /// </summary>
+    [TestFixture]
+    public class WindowsTests : CoreTests { }
+}
